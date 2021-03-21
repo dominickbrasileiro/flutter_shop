4ba48341f8325491f97102e57e9824fdf1832807
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2021-03-21 17:27:59 -0300

    extract product grid to a widget

--- lib/views/products_overview_screen.dart
+++ lib/views/products_overview_screen.dart
@@ -1,31 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_shop/providers/products.dart';
-import 'package:flutter_shop/widgets/product_item.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_shop/widgets/product_grid.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final productsProvider = Provider.of<Products>(context);
-    final products = productsProvider.items;
-
     return Scaffold(
       appBar: AppBar(
         title: Text('MyShop'),
       ),
-      body: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-        padding: const EdgeInsets.all(10),
-        itemCount: products.length,
-        itemBuilder: (ctx, index) => ProductItem(
-          product: products[index],
-        ),
-      ),
+      body: ProductGrid(),
     );
   }
 }
